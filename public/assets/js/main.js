85e1492df1df1f34966eddb3f17aa317b35bce51
LoganTown <ltowne@westmont.edu>
2022-06-05 12:28:41 -0700

    made sending messages cleaner, renamed lobby, input some jquery stuff, added jquery UI

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -9,17 +9,20 @@ function getIRIParameterValue(requestedKey){
             return value;
         }
     }
+    return null;
 }
 
 let username = decodeURI(getIRIParameterValue('username'));
-if ((typeof username == 'undefined') || (username === null)){
+if ((typeof username == 'undefined') || (username === null) || (username === 'null')) {
     username = "Anonymous_"+Math.floor(Math.random()*1000);
 }
 
 
 //$('#messages').prepend('<br>'+username+':</br>');
-let chatRoom = 'Lobby';
-
+let chatRoom = decodeURI(getIRIParameterValue('game_id'));
+if ((typeof chatRoom == 'undefined') || (chatRoom === null) || (chatRoom === 'null')) {
+    chatRoom = "Lobby";
+}
 
 
 //set up the socket.io connection to the server
@@ -37,8 +40,11 @@ socket.on('join_room_response', (payload) => {
         console.log(payload.message);
         return;
     }  
-    let newString = '<p class=\'join_room_response\'>'+payload.username+' joined the '+payload.room+'. (There are '+payload.count+' users in this room)</p>';
-    $('#messages').prepend(newString);
+    let newHTML = '<p class=\'join_room_response\'>'+payload.username+' joined the '+payload.room+'. (There are '+ payload.count+' users in this room)</p>';
+    let newNode = $(newHTML);
+    newNode.hide()
+    $('#messages').prepend(newNode);
+    newNode.show("fade",500);
 })
 
 function sendChatMessage(){
@@ -47,29 +53,43 @@ function sendChatMessage(){
     request.username = username;
     request.message = $('#chatMessage').val();
     console.log('**** Client log message, sending \'send_chat_message\' command: '+JSON.stringify(request));
-    socket.emit('send_chat_message',request);
+    socket.emit('send_chat_message', request);
 }
 
 socket.on('send_chat_message_response', (payload) => {
-    if(( typeof payload == 'undefined') || (payload === null)){
+    if (( typeof payload == 'undefined') || (payload === null)){
         console.log('Server did not send a payload');
         return;
     }
     if(payload.result === 'fail'){
         console.log(payload.message);
         return;
-    }  
-    let newString = '<p class=\'chat_message\'><b>'+payload.username+'</b>:  '+payload.message+'</p>';
-    $('#messages').prepend(newString);
+    }  //something is broken where chat messages arent showing up, not even enter lobby
+    let newHTML = '<p class=\'chat_message\'><b>' + payload.username + '</b>:  ' + payload.message + '</p>';
+    let newNode = $(newHTML);
+    newNode.hide();
+    $('#messages').prepend(newNode);
+    newNode.show("fade", 500);
 })
 
 
 /* request to join chatroom*/
-$( () => {
+$(() => {
     let request = {};
     request.room = chatRoom;
     request.username = username;
     console.log('**** Client log message, sending \'join_room\' command: '+JSON.stringify(request));
     socket.emit('join_room',request);
+
+    $('#lobbyTitle').html(username + "'s Lobby");
+
+    $('#chatMessage').keypress(function (e) {
+        let key = e.which;
+        if (key == 13) { //enter key
+          $('button[id = chatButton]').click();
+          return false;
+        }
+    })
+
 });
 
